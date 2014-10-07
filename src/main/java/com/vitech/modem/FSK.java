20483812ef13eb842b1b6923fde8625ed9981236
Victor Sarapin <victor.sarapin@vitech.com.ua>
2014-10-07 19:45:44 +0300

    Established methods for FSK frame analysis

--- src/main/java/com/vitech/modem/FSK.java
+++ src/main/java/com/vitech/modem/FSK.java
@@ -1,7 +1,23 @@
 package com.vitech.modem;
 
+import java.util.logging.Logger;
+
 /**
- * @see https://github.com/kamalmostafa/minimodem/blob/master/src/fsk.c
+ * @see <a href="https://github.com/kamalmostafa/minimodem/blob/master/src/fsk.c">source code</a>
  */
 public class FSK {
+    class FrameAnalysisInfo {
+    }
+
+    class BitAnalysisInfo {
+    }
+
+    private Logger log = Logger.getLogger(FSK.class.getName());
+
+    public void findFrame(float confidenceSearchLimit, String expectBitsString) {}
+    public int detectCarrier(float minMagnitudeThreshold) { return 0;   }
+
+    private void setTonesByBandshift(int mark, int bandShift) {}
+    private BitAnalysisInfo bitAnalyze() {  return null;    }
+    private FrameAnalysisInfo frameAnalyze() {  return null;    }
 }
