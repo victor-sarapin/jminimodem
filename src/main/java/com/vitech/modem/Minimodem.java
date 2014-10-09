2e11b1b7c6da61646f9e998dc9ac82ba2adb722e
Victor Sarapin <victor.sarapin@vitech.com.ua>
2014-10-09 11:57:59 +0300

    Implemented bit and frame analysis for confidence, finding frame position. Still need to add tests.

--- src/main/java/com/vitech/modem/Minimodem.java
+++ src/main/java/com/vitech/modem/Minimodem.java
@@ -3,7 +3,10 @@ package com.vitech.modem;
 /**
  * Created by vic on 10/1/14.
  *
- * @see https://github.com/kamalmostafa/minimodem/blob/master/src/minimodem.c
+ * @see <a href="https://github.com/kamalmostafa/minimodem/blob/master/src/minimodem.c">source code</a>
  */
 public class Minimodem {
+    public void demodulate() {
+
+    }
 }
